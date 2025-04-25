eb7a47e31d82c0c0065a603bef4cd8ca96cf54b1
Bindhu Owen <bindhuowen@gmail.com>
2025-04-25 20:59:20 +0700

    Prak 6

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/produk', [ProductController::class, 'index']);
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+Route::get('home', function () {
+    return view('home');
+});
