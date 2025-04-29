dea9914f61b045ac15213f1777ae695a61f24d8d
Reynoldsteven <reynoldsteven2506@gmail.com>
2025-04-29 20:30:02 +0700

    view produk, deskripsi dan gambar-gambarnya

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -3,11 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    <title>Flowbite Example</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@1.5.0/dist/flowbite.js"></script>
 </head>
-<body>
-    
+<body style="background: linear-gradient(to bottom left, #FCE8D5, #5EA9B7, #E0F7F9); min-height: 100vh;">
+
+<!-- New Flower -->
+<h1 class="text-5xl ml-28 mt-16 font-semibold text-black">
+    Temukan <span class="text-[#2E2E2E]">Buket Bunga</span> terbaik 
+    <span class="block">untuk setiap momen spesial.</span>
+</h1>
+
+<!-- Start Card 1 -->
+<h3 class="text-2xl ml-28 mt-40 font-semibold text-black">
+    Temukan <span class="text-[#2E2E2E]">buket</span> terbaru kami.
+</h3>
+
+<div class="relative w-[445px] h-[510px] ml-28 mt-8 rounded-3xl shadow-xl overflow-hidden">
+
+    <img class="object-cover w-full h-full" src="{{ asset('image/bukethand1.png') }}" alt="product image" />
+
+    <div class="absolute top-2 left-2 w-full px-9 py-8 text-black">
+        <h3 class="text-2xl mb-2 font-bold tracking-tight">Bouquet Hand</h3>
+        <h4 class="text-xl font-semibold tracking-tight">Nama Bunga</h4>
+
+
+        <div class="flex items-center justify-between ">
+            <span class="text-3xl font-semibold">$599</span>
+        </div>
+    </div>
+</div>
+<!-- End Card 1 -->
+
+<!-- Start Card 2 -->
+
+
+
+<!-- End New Flower -->
+
+
+
+
 </body>
 </html>
