42aa1a8cc91d9a379e72afe0df0ec4d264706068
Reynoldsteven <reynoldsteven2506@gmail.com>
2025-05-04 22:17:44 +0700

    update landing page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,33 +4,39 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
-        
-        // Debug detail untuk setiap produk
-        foreach($products as $product) {
-            \Log::info('Product Debug:', [
-                'id' => $product->id,
-                'name' => $product->name,
-                'image' => $product->image,
-                'image_url' => $product->image_url,
-                'category' => $product->category
-            ]);
+        try {
+            $products = Product::all();
+            
+            // Debug detail untuk setiap produk
+            foreach($products as $product) {
+                Log::info('Product Debug:', [
+                    'id' => $product->id,
+                    'name' => $product->name,
+                    'image' => $product->image,
+                    'image_url' => $product->image_url ?? 'No image URL',
+                    'category' => $product->category ?? 'No category'
+                ]);
+            }
+            
+            // Debug URL gambar
+            foreach($products as $product) {
+                Log::info('Product Image URL:', [
+                    'name' => $product->name,
+                    'image_url' => $product->image_url
+                ]);
+            }
+            
+            return view('welcome', compact('products'));
+        } catch (\Exception $e) {
+            Log::error('Error in HomeController@index: ' . $e->getMessage());
+            return view('welcome', ['products' => collect([])]);
         }
-        
-        // Debug URL gambar
-        foreach($products as $product) {
-            \Log::info('Product Image URL:', [
-                'name' => $product->name,
-                'image_url' => $product->image_url
-            ]);
-        }
-        
-        return view('welcome', compact('products'));
     }
     
     public function contact()
